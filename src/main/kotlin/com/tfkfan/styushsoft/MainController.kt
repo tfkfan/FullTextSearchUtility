6896f5cb47a1e76b39b929bc01de6b8f43358924
Бальцер Артем Александрович <abaltser@alfabank.ru>
2023-08-26 14:13:47 +0300

    added some fixes

--- src/main/kotlin/com/tfkfan/styushsoft/MainController.kt
+++ src/main/kotlin/com/tfkfan/styushsoft/MainController.kt
@@ -16,6 +16,7 @@ import java.io.IOException
 import java.io.InputStream
 import java.util.*
 import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
 
 
@@ -33,6 +34,9 @@ class MainController {
     private lateinit var search: Button
 
     @FXML
+    private lateinit var stop: Button
+
+    @FXML
     private lateinit var searchText: TextArea
 
     @FXML
@@ -44,107 +48,134 @@ class MainController {
     @FXML
     private lateinit var recursiveSearchCheckbox: CheckBox
 
-    private val executorService = Executors.newFixedThreadPool(8)
+    private var executorService = Executors.newFixedThreadPool(8)
     private var directory: File? = null
     private val processed = AtomicInteger(0)
     private val found = AtomicInteger(0)
     private val total = AtomicInteger(0)
+    private val stopped = AtomicBoolean(false)
 
     @FXML
     private fun onDirectorySearchClick() {
-        val fileChooser = DirectoryChooser();
+        val fileChooser = DirectoryChooser()
         directory = fileChooser.showDialog(null)
         directorySearchInput.text = directory?.absolutePath
     }
 
     @FXML
+    private fun onStopClick() {
+        stopped.set(true)
+        executorService.shutdownNow()
+    }
+
+    @FXML
     private fun onSearchClick() {
         if (directory == null) return
+        foundLabel.text = ""
+        stopped.set(false)
+        processed.set(0)
+        found.set(0)
+        updateProgress(true)
+        foundDocuments.children.clear()
+        foundDocuments.isVisible = true
 
-        val text = searchText.text
-        try {
-            val files = ArrayList<File>()
+        executorService = Executors.newFixedThreadPool(8)
+        executorService.submit {
+            val text = searchText.text
+            try {
+                val files = ArrayList<File>()
 
-            if (recursiveSearchCheckbox.isSelected) {
-                val fileStack = Stack<File>()
-                directory!!.listFiles()?.let { fileStack.addAll(it) }
-                while (!fileStack.isEmpty()) {
-                    val file = fileStack.pop()
-                    if (file.isDirectory) file!!.listFiles()?.let { fileStack.addAll(it) }
-                    else if (file.isFile) files.add(file)
-                }
-            } else {
-                directory!!.listFiles()?.let { files.addAll(it) }
-            }
+                if (recursiveSearchCheckbox.isSelected) {
+                    val fileStack = Stack<File>()
+                    directory!!.listFiles()?.let { fileStack.addAll(it) }
+                    while (!fileStack.isEmpty()) {
+                        val file = fileStack.pop()
+                        if (file.isDirectory) file!!.listFiles()?.let { fileStack.addAll(it) }
+                        else if (file.isFile) files.add(file)
+                    }
+                } else
+                    directory!!.listFiles()?.let { files.addAll(it) }
 
-            processed.set(0)
-            found.set(0)
-            total.set(files.size)
-            updateProgress(true)
-            foundDocuments.children.clear()
-            foundDocuments.isVisible = true
+                total.set(files.size)
 
-            files.forEach { file ->
-                if (file.isFile)
+                for (file in files) {
+                    if (stopped.get())
+                        break
+                    if (!file.isFile) {
+                        processed.incrementAndGet()
+                        Platform.runLater {
+                            updateProgress()
+                        }
+                        continue
+                    }
+
                     executorService.submit {
-                        val inputString = try {
-                            println(file.extension)
-                            when (file.extension) {
-                                "doc" -> {
-                                    WordExtractor(HWPFDocument(file.inputStream())).text
+                        try {
+
+                            val inputString = try {
+                                println(file.extension)
+                                when (file.extension) {
+                                    "doc" -> {
+                                        WordExtractor(HWPFDocument(file.inputStream())).text
+                                    }
+
+                                    "docx" -> {
+                                        XWPFWordExtractor(XWPFDocument(file.inputStream())).text
+                                    }
+
+                                    else -> {
+                                        val inputStream: InputStream = file.inputStream()
+                                        inputStream.bufferedReader().use { it.readText() }
+                                    }
                                 }
-                                "docx" -> {
-                                    XWPFWordExtractor(XWPFDocument(file.inputStream())).text
+                            } catch (e: Exception) {
+                                processed.incrementAndGet()
+                                Platform.runLater {
+                                    updateProgress()
                                 }
-                                else -> {
-                                    val inputStream: InputStream = file.inputStream()
-                                    inputStream.bufferedReader().use { it.readText() }
+                                return@submit
+                            }
+                            if (inputString.contains(text, true)) {
+                                found.incrementAndGet()
+                                Platform.runLater {
+                                    val link = Hyperlink(file.absolutePath)
+                                    foundDocuments.children.add(link)
+                                    link.setOnAction { _: ActionEvent ->
+                                        try {
+                                            if (Desktop.isDesktopSupported()) {
+                                                try {
+                                                    Desktop.getDesktop().open(file)
+                                                } catch (ex: IOException) {
+                                                    println(ex)
+                                                }
+                                            }
+                                        } catch (e: Exception) {
+                                            println(e)
+                                        }
+                                    }
                                 }
                             }
                         } catch (e: Exception) {
                             println(e)
-                            val inputStream: InputStream = file.inputStream()
-                            inputStream.bufferedReader().use { it.readText() }
-                        }
-                        if (inputString.contains(text, true)) {
-                            found.incrementAndGet()
-                            Platform.runLater {
-                                val link = Hyperlink(file.absolutePath)
-                                foundDocuments.children.add(link)
-                                link.setOnAction { _: ActionEvent ->
-                                    try {
-                                        if (Desktop.isDesktopSupported()) {
-                                            try {
-                                                Desktop.getDesktop().open(file)
-                                            } catch (ex: IOException) {
-                                                println(ex)
-                                            }
-                                        }
-                                    } catch (e: java.lang.Exception) {
-                                        println(e)
-                                    }
-                                }
-                            }
                         }
                         processed.incrementAndGet()
                         Platform.runLater {
                             updateProgress()
                         }
                     }
+                }
+            } catch (ex: Exception) {
+                println(ex)
             }
-        } catch (ex: Exception) {
-            ex.fillInStackTrace()
         }
     }
 
     private fun updateProgress(setZero: Boolean = false) {
         synchronized(loader) {
             foundLabel.isVisible = true
-            foundLabel.text = "Найдено в ${found.get()} документах"
-            if (setZero)
-                loader.progress = 0.0
-            else
-                loader.progress = processed.get() / total.get().toDouble() + 1 / total.get().toDouble()
+            foundLabel.text = "Найдено в ${found.get()} документах из ${total.get()}"
+            if (setZero) loader.progress = 0.0
+            else loader.progress = processed.get() / total.get().toDouble() + 1 / total.get().toDouble()
         }
     }
 }
